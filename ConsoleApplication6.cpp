4c5cae311cc14f43e6d434a2cc2cf0922343cb6f
vsem1rniy <xdarkrave1337@gmail.com>
2019-11-09 00:45:17 +0200

    first commit

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "math.h"
+using namespace std;
+int main() {
+	int x, t = 5;
+	printf("ERROR404 = ");
+	scanf_s("%d", &x);
+	float result;
+	int dob = 0;
+	for (int n = 1; n < 6; n++)
+	{
+		dob = dob * log(x) * (n * x);
+	}
+	result = dob + 2 * pow(x, 2.0);
+	printf("w = %.3f\n", result);
+	return 0;
+}
+
+
